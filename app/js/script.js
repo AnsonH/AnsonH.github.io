004404f2d81eb843d0ee8891276803ce0461b0b1
AnsonH <ansonheung2000@gmail.com>
2021-03-31 22:34:14 +0800

    Navbar - Add mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,21 @@
 const header = document.querySelector(".header");
 const hamburger = document.querySelector("#hamburger");
+const navLinks = document.querySelectorAll(".nav__links > li > a");
+const navLinksMobile = document.querySelectorAll(".nav__mobile_links > li > a");
 
 // Toggling hamburger icon in navbar
 hamburger.addEventListener("click", function () {
   header.classList.toggle("open");
 });
 
+// Close mobile menu after clicking on link
+navLinksMobile.forEach(function (element) {
+  element.addEventListener("click", function () {
+    header.classList.remove("open");
+  });
+});
+
 // Remove focus after clicking for buttons & anchor links
-const navLinks = document.querySelectorAll(".nav__links > li > a");
 const focusElements = [...navLinks, hamburger];
 focusElements.forEach(function (element) {
   element.addEventListener("click", function () {
